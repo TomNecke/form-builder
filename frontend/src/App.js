4be64ae5af26d28f7ddb214ec423a83b0c90cde3
Tom Necke <tomnecke.pro@gmail.com>
2026-01-17 14:22:24 +0100

    routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom" 
+import Home from './routes/Home/Home.js';
+import Login from './routes/Login/Login.js';
+
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
