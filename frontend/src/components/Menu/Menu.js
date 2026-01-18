d152635160bfb6ec74e3083ed41c6f3d9d79c059
Tom Necke <tomnecke.pro@gmail.com>
2026-01-18 13:11:40 +0100

    2

--- frontend/src/components/Menu/Menu.js
+++ frontend/src/components/Menu/Menu.js
@@ -5,6 +5,8 @@ import { NavLink } from 'react-router-dom'
 export default function Menu() {
     return (
         <div className='Menu'>
+            <label htmlFor='menuBtn'>x</label>
+            <input type='button' id='menuBtn' />
             <ul>
                 <li><NavLink to="/">Home</NavLink></li>
                 <li><NavLink to="/login">Login</NavLink></li>
